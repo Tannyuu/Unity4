584df0ca28cacc917113fed903070e1d1fb97dfe
hogehoge <hogehoge@gmail.com>
2022-02-17 11:06:04 +0900

    Unity4(Raycastの練習)

---
+++ Assets/Shooter.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooter : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            //RaycastHit hit;//RaycastHit構造体型 渡すときは初期値0　使う時に値が入る
+
+            /*
+            if (Physics.Raycast(ray, out hit, 100f))//out 参照渡し 第二引数にぶつかった物が書き込まれる　true false 第三引数は距離
+            {
+                Destroy(hit.collider.gameObject);
+            }
+            
+            
+              foreach(RaycastHit  hit in Physics.RaycastAll(ray)) {
+               Destroy(hit.collider.gameObject);
+              }
+            
+            */
+              foreach (RaycastHit hit in Physics.SphereCastAll(ray,3f)) {
+               Destroy(hit.collider.gameObject);
+              }
+            
+        }
+    }
+}
---
+++ Assets/SphereGenerator.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SphereGenerator : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int z = 0; z < 10; z++)
+        {
+            for (int y = 0; y < 10; y++)
+            {
+                for (int x = 0; x < 10; x++)
+                {
+                    GameObject obj = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+                    obj.transform.position = new Vector3((float)x, (float)y, (float)z);
+                }
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
